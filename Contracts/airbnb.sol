4d32b60635bdd1e5e7ea80c0b1bed1281fd4b447
f20212948 <f20212948@hyderabad.bits-pilani.ac.in>
2023-04-19 00:12:42 +0530

    intial

---
+++ Contracts/airbnb.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract DecentralizedAirBNB{
+    address public admin;
+    uint256 public listing_fee;
+    uint256 private listing_id;
+
+    struct Propery_intfo{
+        //propId,Owner,propName,city,price per day,description
+        uint256 poprId;
+        address Owner;
+        string propName;
+        string city;
+        uint256 pricePerDay;
+    }
+}
